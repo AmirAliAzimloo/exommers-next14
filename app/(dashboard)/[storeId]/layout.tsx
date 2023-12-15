f5a7c100bc67376210ede6d1e702d0b8cdbdb810
Amir Ali <amiraliazimloo123@gmail.com>
2023-12-15 22:59:51 +0330

    base html of navbar completed

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,7 +1,9 @@
-import Navbar from "@/components/navbar";
-import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+
+import Navbar from "@/components/navbar";
+import prismadb from "@/lib/prismadb";
 
 
 export default async function DashboardLayout(
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,24 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
     return ( 
-        <>
-        <div>this will be navbar</div>
-        </>
+       <div className="border-b" >
+        <div className="flex h-16 items-center px-4 " >
+
+
+        <div>this will be a store switcher</div>
+
+
+        <div>this will be the routes</div>
+
+
+        <div className="ml-auto flex items-center space-x-4"  >
+        <UserButton />
+        </div>
+
+
+        </div>
+       </div>
      );
 }
  
