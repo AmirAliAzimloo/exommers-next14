e5a2b2e47b20d4c08526ed89b4b735240e15299e
Amir Ali <amiraliazimloo123@gmail.com>
2023-12-25 23:28:46 +0330

    image-upload config for cloudinary updated

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="ofghneo9">
         {({ open }) => {
           const onClick = () => {
             open();
