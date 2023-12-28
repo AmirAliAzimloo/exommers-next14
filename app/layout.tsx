50faabdbc123a5a657029f1c5c9ea9f0bb2d3192
Amir Ali <amiraliazimloo123@gmail.com>
2023-12-28 22:54:45 +0330

    start refactoring project

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en"> 
         <body className={inter.className}>
           <ToasterProvider />
           <ModalProvider />
