a84c6db46398567693d1407b5e38717f04e3dddc
Amir Ali <amiraliazimloo123@gmail.com>
2024-01-11 19:33:25 +0330

    refactor project

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -25,7 +25,7 @@ const BillboardsPage = async ({
     createdAt: format(item.createdAt, 'MMMM do, yyyy'), 
   }));
 
-  return (
+  return (  
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <BillboardClient data={formattedBillboards} />
