f21a1edb1c3e7eeda6664b9f739b5104870457bb
Amir Ali <amiraliazimloo123@gmail.com>
2024-01-12 14:21:34 +0330

    products refactored

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -40,6 +40,7 @@ const ProductsPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
+        
         <ProductsClient data={formattedProducts} />
       </div>
     </div>
